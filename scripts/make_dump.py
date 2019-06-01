0f6d328b4c24a49711e10a7daddb39c345bb384a
Satoru Nakamura <na.kamura.1263@gmail.com>
2019-06-01 16:50:44 +0900

    test

--- scripts/make_dump.py
+++ scripts/make_dump.py
@@ -1,81 +1,102 @@
 # -*- coding: utf-8 -*-
 
-# Description: retrieves the date (year or century) of items
+# Description: downloads the smallest cropped image captures from a list of items
 # Example usage:
-#   python get_dates.py ../data/src/pd_items.json ../data/dates.json ../data/item_dates.json year
-#   python get_dates.py ../data/src/pd_items.json ../data/centuries.json ../data/item_centuries.json century
+#   python download_images.py ../data/src/pd_items.json ../img/items/ b
+# Derivative types:
+#   t: 150
+#   b: 100 (cropped)
+#   f: 192
+#   r: 300
+#   w: 760
+#   q: 1600
+#   v: 2560
+#   g: Original
 
-from collections import Counter
+import base64
 import json
-import math
-from pprint import pprint
-import re
+import os
+from PIL import Image
 import sys
-from SPARQLWrapper import SPARQLWrapper
 import urllib.request
-
-OUTPUT_FILE = "../data/src/pd_items.json"
-
-loop_flg = True
-page = 1
-
-api_url = "http://iiif2.dl.itc.u-tokyo.ac.jp/api"
+import time
+import requests
+import csv
 
 arr = []
 
-while loop_flg:
-    url = api_url + "/items?property%5B0%5D%5Bjoiner%5D=and&property%5B0%5D%5Bproperty%5D=49&property%5B0%5D%5Btype%5D=neq&property%5B0%5D%5Btext%5D=http%3A%2F%2Fdl.ndl.go.jp%2Fja%2Fiiif_license.html&page=" + str(page)
-    print(url)
+f = open('data/id.csv', 'r')
 
-    page += 1
-
-    request = urllib.request.Request(url)
-    response = urllib.request.urlopen(request)
-
-    response_body = response.read().decode("utf-8")
-    data = json.loads(response_body.split('\n')[0])
-
-    if len(data) > 0:
-        for i in range(len(data)):
-
-            item = data[i]
-
-            obj = {}
-            obj["s"] = item["dcterms:identifier"][0]["@value"]
-            obj["label"] = item["dcterms:title"][0]["@value"] if "dcterms:title" in item else "[No Title]"
-            obj["c_label"] = item["dcndl:digitizedPublisher"][0]["@value"] if "dcndl:digitizedPublisher" in item else  ""
-            obj["type"] = item["dcterms:rights"][0]["@id"] if "dcterms:rights" in item else ""
-            if "dcterms:references" not in item:
-                continue
-            obj["manifest"] = item["dcterms:references"][0]["@id"]
-            obj["date"] = item["uterms:databaseLabel"][0]["@value"] if "uterms:databaseLabel" in item else ""
-            imageURL = ""
-
-            if len(item["o:media"]) > 0:
- 
-                mid = item["o:media"][0]["@id"]
-                imageURL = mid
-
-                """
-
-                request = urllib.request.Request(mid)
-                response = urllib.request.urlopen(request)
-
-                response_body = response.read().decode("utf-8")
-                media = json.loads(response_body.split('\n')[0])
-
-                imageURL = media["o:thumbnail_urls"]["medium"]
-                """
-            else:
-                imageURL = item["dcterms:source"][0]["@id"] if "dcterms:source" in item else ""
-
-            obj["image"] = imageURL
+reader = csv.reader(f)
+header = next(reader)
 
 
-            arr.append(obj)
-    else:
-        loop_flg = False
+map = {}
+result = {}
 
-with open(OUTPUT_FILE, 'w') as outfile:
+for row in reader:
+    map[row[0]] = row[1]
+    result[row[1]] = {
+        "s": row[1]
+    }
+f.close()
+
+print("# Image")
+
+f = open('data/image.csv', 'r', encoding="utf8", errors='ignore')
+
+reader = csv.reader(f)
+header = next(reader)
+
+for row in reader:
+
+    imageURL = row[1]
+    captureId = row[0]
+
+    if captureId in result:
+        result[captureId]["image"] = imageURL
+
+f.close()
+
+# タイトル
+
+files = {}
+files["title"] = "label"
+files["database"] = "c_label"
+files["publisher"] = "date"
+files["right"] = "type"
+files["manifest"] = "manifest"
+
+for key in files:
+
+    print("# "+key)
+
+    f = open('data/'+key+'.csv', 'r')
+
+    reader = csv.reader(f)
+    header = next(reader)
+
+    arr = []
+
+    for row in reader:
+
+        value = row[1]
+
+        captureId = row[0]
+
+        if captureId in map:
+
+            captureId = map[captureId]
+
+            if captureId in result:
+                result[captureId][files[key]] = value
+
+
+# まとめ
+
+for key in result:
+    arr.append(result[key])
+
+with open("../data/src/pd_items.json", 'w') as outfile:
     json.dump(arr, outfile, ensure_ascii=False, indent=4,
               sort_keys=True, separators=(',', ': '))
